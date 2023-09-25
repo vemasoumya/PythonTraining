8f66da0e7f3e7e8b7578a8fbe1a937a09938becd
manish kumar <makum@microsoft.com>
2023-09-25 22:33:36 +0530

    added changes

--- Session1/Basics/basic.py
+++ Session1/Basics/basic.py
@@ -1,6 +1,58 @@
 
-a = 1
-b = 2.3
+#Interpreted language
+
+temp = 60
+if temp == 10:
+    print("fine")
+
+#ndentation Matters
+
+temp = 60
+if temp == 10:
+    print(temp)
+#print("fine")
+
+#dynamic typed language
+x = 5
+y = 3.45
+name = "Alice"
+
+print(x)
+print(y)
+print(name)
+
+#Mixed type Lists
+mixed_list = [1, "two", 3.0, True]
+
+
+#changing Variable type 
+age = 25
+print(age)  # age is an integer
+
+age = "Twenty-five"
+print(age)  # age is now a string
+
+
+#String small changes 
+
+#Concatenation
+
+str1 = "Hello"
+str2 = "World"
+result = str1 + " " + str2  
+print(result)
+
+#String Length:
+text = "Python is great!"
+length = len(text)
+print("Length:", length)
+
+#String Formatting:
+name = "Alice"
+age = 30
+formatted_str = f"My name is {name} and I am {age} years old."
+print(formatted_str)
+
 
 # :- Slice Operator
 # name = 'microsoft'
