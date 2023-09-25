a93414ce1e68f3adc9fedd4a7f5ce8265ff0342c
Soumya Vema <sovema@microsoft.com>
2023-09-25 14:07:41 +0530

    adding exception codes

---
+++ Session1/Basics/exception1.py
@@ -0,0 +1,17 @@
+def divide_numbers(num1, num2):   
+    return num1/num2 
+
+if (__name__ == '__main__'):
+    try:  
+        num1 = 10
+        num2 = 5      
+        if num1 == 1:
+            raise Exception("num1 is 1")
+        print(divide_numbers(1,0))        
+    except ZeroDivisionError as e:    
+        print('Sorry, something went wrong: ', e)
+    except :    
+        print('any other exception')
+    finally:
+        print('This always runs on success or failure')
+
